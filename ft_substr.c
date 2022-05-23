0a4ff8b6961e9f7e23d41f918287d2a35b92dc96
Sergio Garcia <segarcia@student.42heilbronn.de>
2022-05-23 13:53:34 +0200

    substr corrected

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: segarcia <segarcia@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 12:49:02 by segarcia          #+#    #+#             */
-/*   Updated: 2022/05/11 11:02:21 by segarcia         ###   ########.fr       */
+/*   Updated: 2022/05/23 13:53:13 by segarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
-	char	*str;
-	size_t	length;
+	size_t	strlen;
 	size_t	rlen;
+	char	*str;
 
 	if (!s)
 		return (NULL);
-	length = ft_strlen(s);
+	strlen = ft_strlen(s);
 	rlen = len;
-	if (length < len)
-		rlen = length;
+	if (len > strlen)
+		rlen = strlen;
 	str = (char *)malloc(sizeof(char) * (rlen + 1));
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (s[0] && s[start + i] && i < rlen && start <= rlen)
+	while (start < strlen && s[start + i] && i < rlen)
 	{
 		str[i] = s[start + i];
 		i++;
